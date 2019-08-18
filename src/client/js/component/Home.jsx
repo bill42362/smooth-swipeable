ff166090e1c6f034fecd955fe743dad4a16e2658
Bill42362 <bill42362@gmail.com>
2019-08-18 22:13:16 +0800

    Done layout of home and items.

--- src/client/js/component/Home.jsx
+++ src/client/js/component/Home.jsx
@@ -5,23 +5,110 @@ import styled from 'styled-components';
 
 export class Home extends React.PureComponent {
   render() {
-    const { index } = this.props;
+    const { data, index } = this.props;
+    const colorName = data[index].id.replace(/-/g, ' ').toUpperCase();
     return (
       <StyledHome>
-        <div>{index}</div>
+        <Header>
+          <Index color={data[index].code}>{colorName}</Index>
+        </Header>
+        <SwipeableWrapper>
+          <Swipeable>
+            {data.map(color => (
+              <SwipeableItemWrapper key={color.id}>
+                <Item color={color.code}>
+                  <Name>{color.id.replace(/-/g, ' ').toUpperCase()}</Name>
+                  <Code>{color.code}</Code>
+                </Item>
+              </SwipeableItemWrapper>
+            ))}
+          </Swipeable>
+        </SwipeableWrapper>
+        <Footer>
+          <Email>bill42362@gmail.com</Email>
+        </Footer>
       </StyledHome>
     );
   }
 }
 
 Home.propTypes = {
+  data: PropTypes.array,
   index: PropTypes.number,
+  setIndex: PropTypes.func,
 };
 
 Home.defaultProps = {
+  data: [],
   index: 0,
+  setIndex: () => null,
 };
 
-const StyledHome = styled.div``;
+const StyledHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-color: #222f3e;
+`;
+
+const Header = styled.div`
+  flex: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Index = styled.div.attrs(({ color }) => ({ style: { color } }))`
+  font-size: 24px;
+  transition: color 0.6s ease;
+`;
+
+const SwipeableWrapper = styled.div`
+  height: 70%;
+  background-color: #576574;
+`;
+const Swipeable = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+`;
+const SwipeableItemWrapper = styled.div`
+  flex: none;
+  width: 100%;
+  height: 100%;
+  padding: 24px 12px;
+`;
+
+const Item = styled.div.attrs(({ color }) => ({
+  style: { backgroundColor: color },
+}))`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  color: #222f3e;
+`;
+
+const Name = styled.div`
+  font-size: 20px;
+`;
+const Code = styled.div`
+  margin-top: 8px;
+  font-size: 14px;
+`;
+
+const Footer = styled.div`
+  flex: auto;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 20px;
+`;
+const Email = styled.div`
+  color: #c8d6e5;
+`;
 
 export default Home;
--- src/client/js/container/Home.js
+++ src/client/js/container/Home.js
@@ -1,17 +1,33 @@
 // Home.js
 import { connect } from 'react-redux';
 
+import { MERGE_OPERATION_DATA } from '../ActionTypes.js';
+import getOperationData from '../selector/getOperationData.js';
 import Home from '../component/Home.jsx';
+
+const selectPath = ['swipeable'];
+const colors = [
+  { id: 'jigglypuff', code: '#ff9ff3' },
+  { id: 'casandora-yellow', code: '#feca57' },
+  { id: 'pastel-red', code: '#ff6b6b' },
+  { id: 'megaman', code: '#48dbfb' },
+  { id: 'wild-caribbean-green', code: '#1dd1a1' },
+];
 
 const mapStateToProps = state => {
   return {
-    state,
+    data: colors,
+    index: getOperationData(state, selectPath, 'index'),
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    setIndex: ({ index }) => dispatch({ type: '', payload: { index } }),
+    setIndex: ({ index }) =>
+      dispatch({
+        type: MERGE_OPERATION_DATA,
+        payload: { selectPath, data: { index } },
+      }),
   };
 };
 
