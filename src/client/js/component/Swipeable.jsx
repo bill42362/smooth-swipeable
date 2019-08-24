918eb1ef2685f8f6477a9acc01b5f86e0b7ef6a8
Bill42362 <bill42362@gmail.com>
2019-08-24 13:50:18 +0800

    Done swipe with one item.

--- src/client/js/component/Swipeable.jsx
+++ src/client/js/component/Swipeable.jsx
@@ -4,16 +4,41 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 export class Swipeable extends React.PureComponent {
-  state = { x: null, y: null };
+  state = {
+    originX: null,
+    originY: null,
+    deltaX: null,
+    deltaY: null,
+  };
+
+  handleToucheStart = e => {
+    e.preventDefault();
+    const [touch] = e.changedTouches;
+    return this.setState({ originX: touch.pageX, originY: touch.pageY });
+  };
 
   handleToucheMove = e => {
     e.preventDefault();
+    const { originX, originY } = this.state;
     const [touch] = e.changedTouches;
-    return this.setState({ x: touch.pageX, y: touch.pageY });
+    return this.setState({
+      deltaX: touch.pageX - originX,
+      deltaY: touch.pageY - originY,
+    });
+  };
+
+  handleToucheEnd = e => {
+    e.preventDefault();
+    return this.setState({
+      originX: null,
+      originY: null,
+      deltaX: 0,
+      deltaY: 0,
+    });
   };
 
   render() {
-    const { x, y } = this.state;
+    const { deltaX, deltaY } = this.state;
     const { children, currentIndex } = this.props;
     const childrenArray = React.Children.toArray(children);
     //const previous = childrenArray[currentIndex - 1] || childrenArray[childrenArray.length - 1];
@@ -21,10 +46,16 @@ export class Swipeable extends React.PureComponent {
     //const next = childrenArray[currentIndex + 1] || childrenArray[0];
     return (
       <StyledSwipeable>
-        <Axis>{`(${x}, ${y})`}</Axis>
-        {React.cloneElement(current, {
-          onTouchMove: this.handleToucheMove,
-        })}
+        <Axis>{`(${deltaX}, ${deltaY})`}</Axis>
+        <ItemWrapper
+          deltaX={deltaX}
+          deltaY={deltaY}
+          onTouchMove={this.handleToucheMove}
+          onTouchStart={this.handleToucheStart}
+          onTouchEnd={this.handleToucheEnd}
+        >
+          {current}
+        </ItemWrapper>
       </StyledSwipeable>
     );
   }
@@ -45,13 +76,20 @@ const StyledSwipeable = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
-  overflow: auto;
+  overflow: hidden;
 `;
 
 const Axis = styled.div`
   position: absolute;
   top: 0px;
   left: 0px;
+`;
+
+const ItemWrapper = styled.div.attrs(({ deltaX, deltaY }) => ({
+  style: { transform: `translate(${deltaX}px, ${deltaY}px)` },
+}))`
+  width: 100%;
+  height: 100%;
 `;
 
 export default Swipeable;
